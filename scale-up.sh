08a4551bb4b33438b0a93eeb39bc7f1119fd536e
John Beynon <john@render.com>
2023-06-27 16:03:51 +0100

    Fix scale up

--- scale-up.sh
+++ scale-up.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
 curl --request POST \
      --url https://api.render.com/v1/services/$SERVICE_ID_TO_SCALE/scale \
      --header 'accept: application/json' \
