16f5a181260d9b4f70ec1646a6c1be7e92442aa3
John Beynon <john@render.com>
2023-06-27 16:26:59 +0100

    Change the instance type up and down too

--- scale-down.sh
+++ scale-down.sh
@@ -2,6 +2,19 @@
 # exit on error
 set -o errexit
 
+# To change the instance type
+curl --request PATCH \
+     --url https://api.render.com/v1/services/$SERVICE_ID_TO_SCALE \
+     --header 'accept: application/json' \
+     --header 'authorization: Bearer '"$RENDER_API_KEY"'' \
+     --header 'content-type: application/json' \
+     --data '
+{
+  "serviceDetails": {
+    "plan": "starter"
+  }
+}'
+
 curl --request POST \
      --url https://api.render.com/v1/services/$SERVICE_ID_TO_SCALE/scale \
      --header 'accept: application/json' \
--- scale-up.sh
+++ scale-up.sh
@@ -2,6 +2,20 @@
 # exit on error
 set -o errexit
 
+# To change the instance type
+curl --request PATCH \
+     --url https://api.render.com/v1/services/$SERVICE_ID_TO_SCALE \
+     --header 'accept: application/json' \
+     --header 'authorization: Bearer '"$RENDER_API_KEY"'' \
+     --header 'content-type: application/json' \
+     --data '
+{
+  "serviceDetails": {
+    "plan": "standard"
+  }
+}'
+
+
 curl --request POST \
      --url https://api.render.com/v1/services/$SERVICE_ID_TO_SCALE/scale \
      --header 'accept: application/json' \
