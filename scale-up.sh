da3acf49942b24678e68479bf81e0dc2db0a4197
John Beynon <john@render.com>
2023-06-27 15:54:43 +0100

    Initial commit

---
+++ scale-up.sh
@@ -0,0 +1,6 @@
+curl --request POST \
+     --url https://api.render.com/v1/services/$SERVICE_ID_TO_SCALE/scale \
+     --header 'accept: application/json' \
+		 --header 'authorization: Bearer '"$RENDER_API_TOKEN"'' \
+     --header 'content-type: application/json' \
+		 --data '{"numInstances":2}'
